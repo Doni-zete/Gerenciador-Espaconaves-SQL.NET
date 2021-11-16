c1bafd6fe810d9be107027c2f095c01cf6fa3171
Doni-zete <donizete112@gmail.com>
2021-11-16 00:21:15 -0300

    adicionando implementaçaõ

--- src/ConroleAcesso/Dao/DaoBase.cs
+++ src/ConroleAcesso/Dao/DaoBase.cs
@@ -11,7 +11,7 @@ namespace ConroleAcesso.Dao
 
         protected DaoBase()
         {
-            con = new SqlConnection(@"Data Source=DEKSTOP-JEPVP1A\SQLEXPRESS;Initial Catalog=EstrelaDaMorte;Integrated Security=True;Connect Timeout=30");
+            con = new SqlConnection(@"Data Source=DESKTOP-JEPVP1A\SQLEXPRESS;Initial Catalog=EstrelaDaMorte;Integrated Security=True;Connect Timeout=30");
         }
 
         protected async Task Insert(string comando)
